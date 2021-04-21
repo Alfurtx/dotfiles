d118c2d0ec6e7a491c98818fb3be2ddbeb8acf6a
fonsi <alfonso.alfurtx@gmail.com>
2021-04-22 00:35:23 +0200

    Update postInstall.sh

--- postInstall.sh
+++ postInstall.sh
@@ -8,12 +8,13 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/lukechilds/zsh-nvm ~/.oh-my-zsh/custom/plugins/zsh-nvm
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 cd
 if [[-e ".zshrc.pre-oh-my-zsh" && -e ".zshrc"]]; then
     rm .zshrc
     mv .zshrc.pre-oh-my-zsh .zshrc
 fi
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 sudo reboot
